db060d2372786edaccef6d67c000c31832cfb7fe
Marina Ruiz <marinaruizrodriguez01@gmail.com>
2022-12-19 10:26:59 +0100

    ruta absoluta

--- audio-converter.py
+++ audio-converter.py
@@ -18,27 +18,19 @@ def debug():
 def convert_audio():
       print ('Iniciando')
       data = request.get_json()
-      temp_file = open("temp.aac", "wb")
+      temp_file = open("/home/marina/dev/audio-converter/temp.aac", "wb")
       decode_string = base64.b64decode(data['base64'])
       temp_file.write(decode_string)
       temp_file.close()
       print (decode_string)
       audio = (
             ffmpeg
-            .input("temp.aac")
-            .output("temp.wav", f='wav', )
+            .input("/home/marina/dev/audio-converter/temp.aac")
+            .output("/home/marina/dev/audio-converter/temp.wav", f='wav', )
             .overwrite_output()
             .run()
       )
-      # data = open("temp.webm", "r")
-      # encoded = base64.b64encode(data)
-
-      # data, samplerate = sf.read("temp.wav")
-      # sf.write("temp.wav", data, samplerate)
-      # with open("temp.wav", "rb") as wav_file:
-      #       wav_data = wave.open(wav_file)
-      #       audio_data = wav_data.readframes(wav_data.getnframes())
-      with open("temp.wav", 'rb') as f:
+      with open("/home/marina/dev/audio-converter/temp.wav", 'rb') as f:
             contents = f.read()
             audio_data = base64.standard_b64encode(contents)
       
@@ -49,8 +41,8 @@ def convert_audio():
       converted_audio = {
             'base64': audio_data
       }
-      os.remove("temp.aac")
-      os.remove("temp.wav")
+      os.remove("/home/marina/dev/audio-converter/temp.aac")
+      os.remove("/home/marina/dev/audio-converter/temp.wav")
       return jsonify(converted_audio)
 
 if __name__ == '__main__':
@@ -59,5 +51,3 @@ if __name__ == '__main__':
       context.load_cert_chain("/etc/ssl/certs/conversational_ugr_es.pem","/etc/ssl/certs/conversational_ugr_es.key")
       CORS(app)
       app.run(host='0.0.0.0',port=5100,ssl_context=context,debug=False)
-      # CORS(app)
-      # app.run(port=5100, debug=False)
